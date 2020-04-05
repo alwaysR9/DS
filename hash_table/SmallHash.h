91a98c798b5ec554fafcd1b3928bc7bbfbfa5b14
zhufangze <zhufangze123@gmail.com>
2020-04-05 14:28:14 +0800

    Update SmallHash.h

--- hash_table/SmallHash.h
+++ hash_table/SmallHash.h
@@ -1,5 +1,5 @@
 // A Simple Hash Table
-// Which speed is 6 times faster than std::unordered_map
+// Which speed is 6 times faster than std::unordered_map (around 50% load factor in SmallHash)
 
 // with limition: the max number of element should less than 500,000 
 // (you can change this by modify the code in construct function)
